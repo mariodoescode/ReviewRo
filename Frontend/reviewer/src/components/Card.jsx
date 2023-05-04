2ee22535d425ca52eeadb9ff2389ca52fd661fe4
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2023-05-04 17:16:37 +0300

    added items to main

--- Frontend/reviewer/src/components/Card.jsx
+++ Frontend/reviewer/src/components/Card.jsx
@@ -2,13 +2,10 @@ import React, { Component } from 'react'
 
 export default function Card(props) {
   
-  
-
-
 
     return (
       <div class="max-w-sm rounded overflow-hidden shadow-lg">
-  <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains" />
+  <img class="w-full" src={props.img} alt="Sunset in the mountains" />
   <div class="px-6 py-4">
     <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
     <p class="text-gray-700 text-base">
--- Frontend/reviewer/src/components/Main.jsx
+++ Frontend/reviewer/src/components/Main.jsx
@@ -2,34 +2,31 @@ import React, { Component } from 'react'
 import Card from './Card'
 
 export default function Main() {
-  
-  const [products, setProducts] = React.useState({})
-  
-  const data = fetch('https://dummyjson.com/products/1')
-    .then(res => res.json())
-    .then(data => console.log(data))
+  const [allProducts, setProducts] = React.useState([])
 
-  
-  const productData = data.map(product => {
-    return (
-      <Card 
-        id = {product.id}
-        title = {product.title}
-        description = {product.description}
-        price = {product.price}
-      />
-    )
-  })
 
+  React.useEffect(() => {
+    fetchData()
+  },[])
+
+  const fetchData = async () => {
+    await fetch('https://dummyjson.com/products')
+                          .then(res => res.json()
+                          .then(data => setProducts(data.products)))
+    console.log(allProducts)
+  }
 
     return (      
-    <body>
-      <div>
-        <pre>{JSON.stringify(products, null, 2)}</pre>
-      </div>
-      <div>
-
-      </div>
-
-    </body>)
+    <div className="flex items-center justify-between">
+      {allProducts?.map(product => (
+        <Card
+        id={product.id} 
+        title={product.title}
+        description={product.description}
+        img={product.thumbnail}
+        />
+        )
+      ) }
+    </div>
+    )
 }
