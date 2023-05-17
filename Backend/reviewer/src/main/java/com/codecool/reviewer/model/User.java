8db688fb0edfcaeffdf5f85b5d066d9addfe5327
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2023-05-17 11:34:15 +0300

    created product, review and user entities

---
+++ Backend/reviewer/src/main/java/com/codecool/reviewer/model/User.java
@@ -0,0 +1,24 @@
+package com.codecool.reviewer.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "\"User\"")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+
+}
