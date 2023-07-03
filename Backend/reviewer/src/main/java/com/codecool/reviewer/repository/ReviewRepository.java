a1f580c16e82e914785c86238e6cf48df17e6032
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2023-07-03 14:00:35 +0300

    renamed import package name for Product

--- Backend/reviewer/src/main/java/com/codecool/reviewer/repository/ReviewRepository.java
+++ Backend/reviewer/src/main/java/com/codecool/reviewer/repository/ReviewRepository.java
@@ -1,6 +1,6 @@
 package com.codecool.reviewer.repository;
 
-import com.codecool.reviewer.model.Review;
+import com.codecool.reviewer.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
