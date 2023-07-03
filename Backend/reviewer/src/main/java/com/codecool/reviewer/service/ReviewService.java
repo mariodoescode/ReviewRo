668f50a823dd4e3db0353b07205082fd12f1cb73
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2023-07-03 14:01:10 +0300

    added reviewservice constructor and addReview function

--- Backend/reviewer/src/main/java/com/codecool/reviewer/service/ReviewService.java
+++ Backend/reviewer/src/main/java/com/codecool/reviewer/service/ReviewService.java
@@ -1,7 +1,29 @@
 package com.codecool.reviewer.service;
 
+import com.codecool.reviewer.dto.ReviewDTO;
+import com.codecool.reviewer.entity.Review;
+import com.codecool.reviewer.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
+
+    ReviewRepository reviewRepository;
+
+
+    @Autowired
+    public ReviewService (ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    public void addReview(ReviewDTO reviewDTO) {
+        reviewRepository.save(new Review(
+                reviewDTO.getId(),
+                reviewDTO.getMessage(),
+                reviewDTO.getRating(),
+                reviewDTO.getProduct(),
+                reviewDTO.getAppUser()
+        ));
+    }
 }
