59c4cc7d672979d26d4f4ed6c9a614e9a080d7bd
Mario Rusu <106011177+mariodoescode@users.noreply.github.com>
2023-07-18 13:46:45 +0300

    fixed reviews button scope

--- Frontend/reviewer/src/components/Card.jsx
+++ Frontend/reviewer/src/components/Card.jsx
@@ -72,28 +72,28 @@ export default function Card(props) {
             <span className="card-rating">5.0</span>
           </div>
         </div>
-        <a  className="card-review-button" >
+        
         <Link
              to="product"
              state={{
-              "id": props.id,
-              "title": props.title,
-              "description": props.description,
-              "img": props.img
-            }}
+               "id": props.id,
+               "title": props.title,
+               "description": props.description,
+               "img": props.img
+              }}
             // state={state}
-          >Reviews</Link>
+          ><a  className="card-review-button" >Reviews</a></Link>
           {/* <svg xmlns="http://www.w3.org/2000/svg"
             className="mr-2 h-6 w-6" 
             fill="none" viewBox="0 0 24 24" 
             stroke="currentColor" strokeWidth="2">
             <path 
-              strokeLinecap="round" 
-              strokeLinejoin="round" 
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" 
             />
           </svg> */}
-        </a>    
+              
         </div>
       </div>
     </div>
