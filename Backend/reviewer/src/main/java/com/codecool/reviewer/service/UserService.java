9bdef0f1991f7e6110f6dab6e354b9d57a9adc66
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2023-08-23 11:03:17 +0300

    get userdata and update description methods

--- Backend/reviewer/src/main/java/com/codecool/reviewer/service/UserService.java
+++ Backend/reviewer/src/main/java/com/codecool/reviewer/service/UserService.java
@@ -1,13 +1,16 @@
 package com.codecool.reviewer.service;
 
 import com.codecool.reviewer.entity.AppUser;
+import com.codecool.reviewer.entity.Review;
+import com.codecool.reviewer.payload.response.UserData;
+import com.codecool.reviewer.repository.ReviewRepository;
 import com.codecool.reviewer.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+import java.util.Optional;
 
 
 @Service
@@ -16,11 +19,13 @@ public class UserService {
     UserRepository userRepository;
 
     PasswordEncoder passwordEncoder;
+    ReviewRepository reviewRepository;
 
 
     @Autowired
-    public UserService (UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService (UserRepository userRepository, ReviewRepository reviewRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.reviewRepository = reviewRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -36,5 +41,18 @@ public class UserService {
 
     public AppUser findUserByUsername(String username) {
         return userRepository.findAppUserByUsername(username);
+    }
+
+
+    public UserData getUserData(String username) {
+        AppUser user = userRepository.findAppUserByUsername(username);
+        List<Review> reviews = reviewRepository.findAllByAppUser_Id(user.getId());
+        return new UserData(reviews,user);
+    }
+
+    public void updateDescription(String description, String username) {
+        AppUser user = userRepository.findAppUserByUsername(username);
+        user.setDescription(description);
+        userRepository.save(user);
     }
 }
