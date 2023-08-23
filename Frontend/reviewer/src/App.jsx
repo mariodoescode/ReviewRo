60f8e08021b659bb47653b71e433936bb022ce00
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2023-08-23 11:06:01 +0300

    changed path for some imports

--- Frontend/reviewer/src/App.jsx
+++ Frontend/reviewer/src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Navbar'
-import Main from './components/Main'
+import Main from './views/Main'
 import { Routes, Route } from 'react-router-dom'
-import About from './components/About'
-import ProductPage from './components/ProductPage'
-import RegisterPage from './components/RegisterPage'
-import Profile from './components/Profile'
-import Login from './components/Login'
+import ProductPage from './views/ProductPage'
+import RegisterPage from './views/RegisterPage'
+import Profile from './views/Profile'
+import Login from './views/Login'
 import React from 'react'
 import { useState } from 'react'
 
@@ -22,11 +21,11 @@ function App() {
   <div className='flex flex-col place-content-center'>
     <Header user={user}/>
     <Routes>
-      <Route exact path='/about' element={<About />}></Route>
       <Route exact path='/' element={<Main />}></Route>
       <Route exact path='/product' element={<ProductPage />}></Route>
       <Route exact path='/register' element={<RegisterPage />}></Route>
       <Route exact path='/profile' element={<Profile />} ></Route>
+      {/* <Route exact path='/edit-profile' element={<Profile />} ></Route> */}
       <Route exact path='/login' element={<Login setValue={setUser} test={"test"}/>}></Route>
     </Routes>
     <Footer />
